ddf9037e5d5ed35cbe5b658f898d85f86cc024c4
LinusBF <linus.webdevelopment@gmail.com>
2017-06-16 01:16:33 +0200

    Added trusted proxies

--- config/app.php
+++ config/app.php
@@ -176,7 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+		
+		Fideloper\Proxy\TrustedProxyServiceProvider::class,
+		
     ],
 
     /*
